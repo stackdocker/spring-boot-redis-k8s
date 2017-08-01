455eaf32ee377a9ed2f04c05f48d822c27a3201d
tangfeixiong <tangfx128@gmail.com>
2017-08-01 06:53:45 +0000

    Application.java both for redis and sentinel, update angular and angular/cli for debian:stretch base-image, docker-compose file

--- src/main/java/com/google/cloudjlb/Application.java
+++ src/main/java/com/google/cloudjlb/Application.java
@@ -1,5 +1,7 @@
 package com.google.cloudjlb;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +10,11 @@ import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisSentinelConfiguration;
-
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
@@ -43,14 +46,40 @@ public class Application {
     };
   }
 
+  @Autowired
+  private Environment env;
 
-
-  @Bean
+  @Bean(name="RedisConnectionFactory")
   public RedisConnectionFactory lettuceConnectionFactory() {
+	if (0 != redisHostName.length() && 0 < redisPort) {
+		return context.getBean(JedisConnectionFactory.class);
+	}
+	String redis = env.getProperty("spring.redis.host");
+	String sentinelMaster = env.getProperty("spring.redis.sentinel.master");
+	String sentinelNodes = env.getProperty("spring.redis.sentinel.nodes");
+	
     RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration().master("master")
         .sentinel("redis-sentinel.default.svc.cluster.local", 26379);
     return new LettuceConnectionFactory(sentinelConfig);
   }
+
+    @Value("${redis.hostname}")
+    private String redisHostName;
+
+    @Value("${redis.port}")
+    private int redisPort;
+
+    @Autowired
+    private ApplicationContext context;
+
+    @Bean(name="RedisConnectionFactory1")
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory factory = new JedisConnectionFactory();
+        factory.setHostName(redisHostName);
+        factory.setPort(redisPort);
+        factory.setUsePool(true);
+        return factory;
+    }
 
   @Bean
   public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
